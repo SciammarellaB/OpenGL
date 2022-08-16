b98b4692b864f471db02cf07fea29f8b24682e72
Brenno Sciammarella <brenno.s97@gmail.com>
2022-08-15 23:09:10 -0300

    Correcoes de identacao e comentario

--- Quadrado/Quadrado.cpp
+++ Quadrado/Quadrado.cpp
@@ -1,25 +1,36 @@
 #include <GL/glut.h>
 
-void displayMe(void)
+//FORMA
+void quadrado(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    
     glBegin(GL_POLYGON);
+        //V1
         glVertex2f(-0.5, 0.5);
+        //V2
         glVertex2f(0.5, 0.5);
+        //V3
         glVertex2f(0.5, -0.5);
+        //V4
         glVertex2f(-0.5, -0.5);
     glEnd();
+
     glFlush();
 }
 
 int main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Quadrado");
-    glutDisplayFunc(displayMe);
-    glutMainLoop();
+    glutInit(&argc, argv);              //INICIALIZADOR DA BIBLIOTECA
+    glutInitDisplayMode(GLUT_RGB);      //PADRAO CASO N SEJA INFORMADO
+
+    glutInitWindowSize(500, 500);       //TAMANHO DA JANELA
+    glutInitWindowPosition(100, 100);   //POSICAO DA JANELA QUANDO EXECUTAR
+
+    glutCreateWindow("Quadrado");       //TITULO DO PROGRAMA
+    
+    glutDisplayFunc(quadrado);          //CALLBACK DA JANELA
+
+    glutMainLoop();                     //LOOP PARA PROCESSAMENTO
     return 0;
 }
--- QuadradoCor/QuadradoCor.cpp
+++ QuadradoCor/QuadradoCor.cpp
@@ -1,25 +1,25 @@
 #include <GL/glut.h>
 
 //FORMA
-void square(void)
+void quadrado(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
     
     glBegin(GL_POLYGON);
-        //RESET COLOR
+    //RESET COLOR
         glClearColor(0.0, 0.0, 0.0, 1.0);
-        //COR       - RGB - COR PARA O VERTICE ABAIXO
-        //VERTICE   -
-        //V1
+    //COR       - RGB - COR PARA O VERTICE ABAIXO
+    //VERTICE   -
+    //V1
         glColor3f(0.0, 255.0, 0.0);
         glVertex2f(-0.5, 0.5);
-        //V2
+    //V2
         glColor3f(0.0, 0.0, 120.0);
         glVertex2f(0.5, 0.5);
-        //V3
+    //V3
         glColor3f(0.0, 255.0, 58.0);
         glVertex2f(0.5, -0.5);
-        //V4
+    //V4
         glColor3f(150.0, 255.0, 0.0);
         glVertex2f(-0.5, -0.5);
     glEnd();
@@ -37,7 +37,7 @@ int main(int argc, char** argv)
 
     glutCreateWindow("Quadrado - Cor"); //TITULO DO PROGRAMA
     
-    glutDisplayFunc(square);            //CALLBACK DA JANELA
+    glutDisplayFunc(quadrado);            //CALLBACK DA JANELA
 
     glutMainLoop();                     //LOOP PARA PROCESSAMENTO
     return 0;
